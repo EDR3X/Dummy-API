885280a293b81bcf992d8e16d79815d522691272
EDR3X <dhunganaannanta@gmail.com>
2022-07-04 19:06:32 +0545

    minor fix

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import controller from "../controller/controller";
 const router = Router();
 
-router.get("/user", controller.userController);
+router.get("/user", controller.usersController);
 router.get("/user/:id", controller.userController);
 
 router.get("/cars", controller.carsController);
