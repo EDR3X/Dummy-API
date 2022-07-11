1f6151f4b14ff587b352d4ef49e3e6a89c311b7d
EDR3X <dhunganaannanta@gmail.com>
2022-07-11 14:50:17 +0545

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 import mainRouter from "./routes/router";
 
